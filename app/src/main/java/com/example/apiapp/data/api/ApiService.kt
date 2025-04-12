91860aa4dda1b025f6321ff44b00af4edd59a816
Sam. <swayams24@gmail.com>
2025-04-12 23:44:35 +1000

    Add POST and GET method

--- app/src/main/java/com/example/apiapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/apiapp/data/api/ApiService.kt
@@ -1,4 +1,18 @@
 package com.example.apiapp.data.api
 
-class ApiService {
+import com.example.apiapp.data.model.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiService {
+    @POST("{location}/auth")
+    suspend fun login(
+        @Path("location") location: String,
+        @Body request: LoginRequest
+    ): Response<LoginResponse>
+
+    @GET("dashboard/{keypass}")
+    suspend fun getDashboard(
+        @Path("keypass") keypass: String
+    ): Response<DashboardResponse>
 }
