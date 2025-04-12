50afcd12a331bc9b07e4fcaaeb6e1a1f0e777a56
Sam. <swayams24@gmail.com>
2025-04-12 23:45:34 +1000

    Setup Hilt dependency injection

--- app/src/main/java/com/example/apiapp/di/AppModule.kt
+++ app/src/main/java/com/example/apiapp/di/AppModule.kt
@@ -1,4 +1,25 @@
 package com.example.apiapp.di
 
-class AppModule {
+import com.example.apiapp.data.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://nit3213api.onrender.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
 }
