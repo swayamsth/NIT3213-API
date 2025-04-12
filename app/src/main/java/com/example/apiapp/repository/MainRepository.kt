d693df29d5063a74abce951ceb387f005ee59e6d
Sam. <swayams24@gmail.com>
2025-04-12 23:45:58 +1000

    implement for API calls

--- app/src/main/java/com/example/apiapp/repository/MainRepository.kt
+++ app/src/main/java/com/example/apiapp/repository/MainRepository.kt
@@ -1,4 +1,10 @@
 package com.example.apiapp.repository
 
-class MainRepository {
+import com.example.apiapp.data.api.ApiService
+import com.example.apiapp.data.model.LoginRequest
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(private val api: ApiService) {
+    suspend fun login(location: String, request: LoginRequest) = api.login(location, request)
+    suspend fun getDashboard(keypass: String) = api.getDashboard(keypass)
 }
