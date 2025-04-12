2ac9fc4b3c16dad77572d39b16a18a2c2a200044
Sam. <swayams24@gmail.com>
2025-04-12 23:52:51 +1000

    Display full entity info

--- app/src/main/java/com/example/apiapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/apiapp/ui/details/DetailsActivity.kt
@@ -1,4 +1,19 @@
 package com.example.apiapp.ui.details
 
-class DetailsActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.apiapp.R
+import com.example.apiapp.data.model.Entity
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        val entity = intent.getSerializableExtra("entity") as Entity
+        findViewById<TextView>(R.id.property1).text = entity.property1
+        findViewById<TextView>(R.id.property2).text = entity.property2
+        findViewById<TextView>(R.id.description).text = entity.description
+    }
 }
