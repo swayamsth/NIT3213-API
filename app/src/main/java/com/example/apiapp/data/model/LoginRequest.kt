3bdfa3c079ed0fed9fbb690ccac3446ca5a78ccd
Sam. <swayams24@gmail.com>
2025-04-14 01:53:06 +1000

    Code Refacto DashboardReponse

--- app/src/main/java/com/example/apiapp/data/model/LoginRequest.kt
+++ app/src/main/java/com/example/apiapp/data/model/LoginRequest.kt
@@ -4,8 +4,3 @@ package com.example.apiapp.data.model
 data class LoginRequest(val username: String, val password: String)
 
 data class LoginResponse(val keypass: String)
-
-data class DashboardResponse(
-    val entities: List<Entity>,
-    val entityTotal: Int
-)
