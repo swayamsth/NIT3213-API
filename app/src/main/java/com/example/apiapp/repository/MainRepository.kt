677133c4214ba2386e55acb3be43c3c2dd921d87
Sam. <swayams24@gmail.com>
2025-04-14 02:22:48 +1000

    add login and dashboard api calls

--- app/src/main/java/com/example/apiapp/repository/MainRepository.kt
+++ app/src/main/java/com/example/apiapp/repository/MainRepository.kt
@@ -4,7 +4,9 @@ import com.example.apiapp.data.api.ApiService
 import com.example.apiapp.data.model.LoginRequest
 import javax.inject.Inject
 
-class MainRepository @Inject constructor(private val api: ApiService) {
+class MainRepository @Inject constructor(
+    private val api: ApiService
+) {
     suspend fun login(location: String, request: LoginRequest) = api.login(location, request)
     suspend fun getDashboard(keypass: String) = api.getDashboard(keypass)
 }
