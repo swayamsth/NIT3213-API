03dc001aecdfe1266eac4664bd4fa5c19be5df16
Sam. <swayams24@gmail.com>
2025-04-14 21:50:55 +1000

    Recieve mealType res and display it on page

--- app/src/main/java/com/example/apiapp/ui/dashboard/EntityAdapter.kt
+++ app/src/main/java/com/example/apiapp/ui/dashboard/EntityAdapter.kt
@@ -22,10 +22,12 @@ class EntityAdapter(
     inner class EntityViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val prop1: TextView = view.findViewById(R.id.prop1)
         private val prop2: TextView = view.findViewById(R.id.prop2)
+        private val mealType: TextView = view.findViewById(R.id.mealType)
 
         fun bind(entity: Entity) {
             prop1.text = entity.dishName
             prop2.text = entity.origin
+            mealType.text = entity.mealType
             itemView.setOnClickListener { onClick(entity) }
         }
     }
