2f2a21a0a5467252097510b6a47a4a1a1759d325
Sam. <swayams24@gmail.com>
2025-04-14 23:15:43 +1000

    Add Back to Dashboard Btn and connectivity to the page

--- app/src/main/java/com/example/apiapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/apiapp/ui/details/DetailsActivity.kt
@@ -1,19 +1,35 @@
 package com.example.apiapp.ui.details
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.apiapp.R
 import com.example.apiapp.data.model.Entity
+import com.example.apiapp.ui.dashboard.DashboardActivity
+import com.google.android.material.button.MaterialButton
 
 class DetailsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val entity = intent.getSerializableExtra("entity") as Entity
-        findViewById<TextView>(R.id.property1).text = entity.dishName
-        findViewById<TextView>(R.id.property2).text = entity.origin
+        val entity = intent.getSerializableExtra("entity") as? Entity ?: return
+
+        findViewById<TextView>(R.id.dishName).text = entity.dishName
+        findViewById<TextView>(R.id.origin).text = "Origin: ${entity.origin}"
+        findViewById<TextView>(R.id.mainIngredient).text = "Main ingredient: ${entity.mainIngredient}"
+        findViewById<TextView>(R.id.mealType).text = entity.mealType
         findViewById<TextView>(R.id.description).text = entity.description
+
+        val dashboardBtn = findViewById<MaterialButton>(R.id.backToDashboardBtn)
+
+        dashboardBtn.setOnClickListener {
+
+            finish()
+        }
+
     }
 }
